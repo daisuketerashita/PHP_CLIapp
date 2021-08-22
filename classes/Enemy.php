1cb9d393ef56c197798dbec5c10815f25f5a3da3
daisuketerashita <ekusiad.tera@icloud.com>
2021-08-22 18:07:30 +0900

    敵クラスの作成

---
+++ classes/Enemy.php
@@ -0,0 +1,25 @@
+<?php
+//敵クラスの作成
+class Enemy{
+    //プロパティ
+    const MAX_HITPOINT = 50; //最大HPの定義　定数
+    public $name; //名前
+    public $hitPoint = 50; //現在のHP
+    public $attackPoint = 10; //攻撃力
+
+    //敵側の攻撃メソッド
+    public function doAttack($human){
+        echo "『".$this->name."』の攻撃！<br>";
+        echo "【".$human->name."】に".$this->attackPoint."のダメージ！<br>";
+        $human->tookDamage($this->attackPoint);
+    }
+
+    //敵側のダメージメソッド
+    public function tookDamage($damage){
+        $this->hitPoint -= $damage;
+        //HPが0未満にならないための処理
+        if($this->hitPoint < 0){
+            $this->hitPoint = 0;
+        }
+    }
+}
--- classes/Human.php
+++ classes/Human.php
@@ -7,14 +7,14 @@ class Human{
     public $hitPoint = 100; //現在のHP
     public $attackPoint = 20; //攻撃力
 
-    //攻撃メソッド
+    //味方側の攻撃メソッド
     public function doAttack($enemy){
         echo "『".$this->name."』の攻撃！<br>";
         echo "【".$enemy->name."】に".$this->attackPoint."のダメージ！<br>";
         $enemy->tookDamage($this->attackPoint);
     }
 
-    //ダメージメソッド
+    //味方側のダメージメソッド
     public function tookDamage($damage){
         $this->hitPoint -= $damage;
         //HPが0未満にならないための処理
